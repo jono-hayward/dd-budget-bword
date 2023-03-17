1d54c7a150d19077a4e7c519b98376c9fa67dcdd
Jono Hayward <jonathon.hayward@gmail.com>
2023-03-17 08:33:23 +0000

    Add preferences to store

--- src/stores.js
+++ src/stores.js
@@ -16,8 +16,12 @@ const stored_budget = store_get( 'localStorage', 'budget' );
 export const budget = writable( stored_budget && JSON.parse( stored_budget ) || [] );
 budget.subscribe( value => store_set( 'localStorage', 'budget', JSON.stringify(value) ) );
 
-
 // Stores user's budget data
 const stored_groceries = store_get( 'localStorage', 'groceries' );
 export const groceries = writable( stored_groceries && JSON.parse( stored_groceries ) || [] );
 groceries.subscribe( value => store_set( 'localStorage', 'groceries', JSON.stringify(value) ) );
+
+// Stores user's preferences
+const stored_prefs = store_get( 'localStorage', 'prefs' );
+export const prefs = writable( stored_prefs && JSON.parse( stored_prefs ) || {} );
+prefs.subscribe( value => store_set( 'localStorage', 'prefs', JSON.stringify(value) ) );

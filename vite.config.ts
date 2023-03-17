f14a5567a77ea4eb8c3ed00bd5deb3d42acf461e
Jono Hayward <jonathon.hayward@gmail.com>
2023-03-17 13:24:28 +0000

    Removed unnecessary https in testing

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,6 @@ import { defineConfig } from 'vite';
 import basicSsl from '@vitejs/plugin-basic-ssl';
 
 export default defineConfig({
-	plugins: [sveltekit(), basicSsl()],
-	server: {
-		https: true
-	}
+	plugins: [sveltekit()],
+
 });
